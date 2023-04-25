5fe4dba27153f792e344ee78b821d9dff98c56e4
YoonjeongLulu <dolls_love@naver.com>
2023-04-25 23:27:06 +0900

    feat: server, client ws example

---
+++ client/src/App.tsx
@@ -0,0 +1,44 @@
+import { React, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState<string>('');
+  const webSocket = new WebSocket('ws://localhost:8080'); // 서버 연결
+
+  // 웹 소켓 연결 이벤트
+  webSocket.onopen = function () {
+    alert('웹소켓 서버와 연결에 성공했습니다.');
+  };
+
+  // 웹 소켓 메세지 수신
+  webSocket.onmessage = function (event) {
+    alert(event.data);
+  };
+
+  // 웹 소켓 연결 종료
+  webSocket.onclose = function () {
+    alert('웹소켓 서버와 연결이 종료되었습니다.');
+  };
+
+  // 오류 발생
+  webSocket.onerror = function (error) {
+    console.log(error);
+  };
+
+  const sendMessage = () => {
+    webSocket.send(message);
+  };
+
+  const onChangeMessage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setMessage(event.target.value);
+  };
+  return (
+    <div>
+      <input type="text" id="message" value={message} onChange={onChangeMessage} />
+      <button type="button" onClick={sendMessage}>
+        전송
+      </button>
+    </div>
+  );
+}
+
+export default App;
