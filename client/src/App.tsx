33e8caa154f1a3776dab7976f648300e1b5c463b
YoonjeongLulu <dolls_love@naver.com>
2023-04-27 23:35:04 +0900

    refactor: 리렌더링 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,8 @@
-import { React, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [message, setMessage] = useState<string>('');
   const webSocket = new WebSocket('ws://localhost:8080'); // 서버 연결
-
-  // 웹 소켓 연결 이벤트
-  webSocket.onopen = function () {
-    alert('웹소켓 서버와 연결에 성공했습니다.');
-  };
 
   // 웹 소켓 메세지 수신
   webSocket.onmessage = function (event) {
@@ -24,20 +19,28 @@ function App() {
     console.log(error);
   };
 
-  const sendMessage = () => {
+  const sendMessage = (e: any) => {
+    e.preventDefault();
     webSocket.send(message);
   };
 
   const onChangeMessage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setMessage(event.target.value);
   };
+
+  useEffect(() => {
+    // 웹 소켓 연결 이벤트
+    webSocket.onopen = function () {
+      alert('웹소켓 서버와 연결에 성공했습니다.');
+    };
+  }, []);
   return (
-    <div>
+    <form>
       <input type="text" id="message" value={message} onChange={onChangeMessage} />
-      <button type="button" onClick={sendMessage}>
+      <button type="submit" onClick={sendMessage}>
         전송
       </button>
-    </div>
+    </form>
   );
 }
 
